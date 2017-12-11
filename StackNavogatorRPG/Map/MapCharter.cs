a7e6f8875a04a87e21fad131c53117098a4392a0
Sean Robb <Sean Robb>
2017-12-11 14:32:44 -0500

    Add random map gen

---
+++ StackNavogatorRPG/Map/MapCharter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MapGenAgentBased
+{
+    public enum Markers
+    {
+        Explored,
+        Occupied,
+        Wall
+    }
+
+    public class MapCharter
+    {
+        public RoomCell[,] Map { get; set; }
+        public int MaxSizeX { get; set; }
+        public int MaxSizeY { get; set; }
+        public byte[] imageBuffer { get; set; }
+
+
+        public MapCharter(RoomCell[,] map, int maxX, int maxY)
+        {
+            Map = map;
+            MaxSizeX = maxX;
+            MaxSizeY = maxY;
+        }
+
+        public byte[] PixelMaker(Markers marker)
+        {
+            switch (marker)
+            {
+                case Markers.Explored: return new byte[4] { 255, 255, 255, 255 };
+                case Markers.Occupied: return new byte[4] { 255, 0, 0, 255 };
+                case Markers.Wall: return new byte[4] { 0, 0, 0, 255 };
+                default: return new byte[4] { 0, 255, 0, 255 };
+            }
+        } 
+
+
+        //create thumbnail
+        public void ChartMap()
+        {
+            int resBytes = (MaxSizeX * 4) * MaxSizeY;
+            imageBuffer = new byte[resBytes];
+
+            for(int x = 0; x < MaxSizeX; x++)
+            {
+                for(int y = 0; y < MaxSizeY; y++)
+                {
+                    RoomCell rm = Map[x, y];
+
+                    if(rm == null)
+                    {
+                        PlotPixel(x, y, PixelMaker(Markers.Wall));
+                    }
+                    else
+                    {
+                        PlotPixel(x, y, PixelMaker(Markers.Explored));
+                    }
+                }
+            }
+
+            unsafe
+            {
+                fixed(byte* ptr = imageBuffer)
+                {
+                    using (Bitmap image = new Bitmap(MaxSizeX, MaxSizeY, MaxSizeX * 4, System.Drawing.Imaging.PixelFormat.Format32bppRgb, new IntPtr(ptr)))
+                    {
+                        image.Save(@"map.png");
+                    }
+                }
+            }
+        }
+
+
+        public void PlotPixel(int x, int y, byte[] pixel)
+        {
+            int offset = ((MaxSizeX * 4) * y) + (x * 4);
+
+            //redundant
+            imageBuffer[offset] = pixel[0];
+            imageBuffer[offset + 1] = pixel[1];
+            imageBuffer[offset + 2] = pixel[2];
+            imageBuffer[offset + 3] = pixel[3];
+
+
+        }
+
+    }
+}
---
+++ StackNavogatorRPG/Map/RoomCell.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapGenAgentBased
+{
+
+    public enum Direction
+    {
+        North,
+        West,
+        South,
+        East
+    }
+
+    //Physical room
+    public class RoomCell
+    {
+        public RoomCell North { get; set; }
+        public RoomCell West { get; set; }
+        public RoomCell South { get; set; }
+        public RoomCell East { get; set; }
+
+        public Treasure Treasure { get; set; }
+        public Statue Statue { get; set; }
+
+        //NEED TO ACTUALLY IMPLEMENT THE IMAGE
+        public int Image { get; set; }
+
+        public bool Visited;
+        public bool Boss;
+
+
+        //Image of room is based off of heading and doors within room
+        //It is represented as a BYTE
+        // 00 - 00
+        // Bottom is the player heading
+        // Top are the doors present within RoomCell
+        public void AssignRoomCellImage(Direction fromRoom)
+        {
+            byte roomLookup = 0x00;
+
+            //ASSIGN BOTTOM OF BYTE
+            switch (fromRoom)
+            {
+                //BIN   HEX     HUMAN
+                //0000  0       NO HEADING
+                //0001  1       NORTH
+                //0010  2       EAST
+                //0100  4       SOUTH
+                //1000  8       WEST
+                case Direction.North: roomLookup = 0x01; break;
+                case Direction.East: roomLookup = 0x02; break;
+                case Direction.South: roomLookup = 0x04; break;
+                case Direction.West: roomLookup = 0x08; break;
+            }
+
+            //ASSIGN TOP OF BYTE
+            //BIN   HEX     HUMAN
+            //0000  0       No Door Exists (should not happen)
+            //0001  1       North Door
+            //0010  2       East Door
+            //0100  4       South Door
+            //1000  8       West Door
+            if(North != null)
+            {
+                roomLookup &= 0x10;
+            }
+            if(East != null)
+            {
+                roomLookup &= 0x20;
+            }
+            if (South != null)
+            {
+                roomLookup &= 0x40;
+            }
+            if(West != null)
+            {
+                roomLookup &= 0x80;
+            }
+
+            //do image lookup here
+            //01010001
+
+
+        }
+
+
+
+    }
+
+
+    //dummy classes
+    public class Treasure
+    {
+
+    }
+
+    public class Statue
+{
+
+}
+}
